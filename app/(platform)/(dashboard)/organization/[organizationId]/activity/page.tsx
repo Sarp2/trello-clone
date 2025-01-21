e64128ea1d7200ee6a87a92e811229a9ab680d3f
Sarp2 <sarppehlivan444@gmail.com>
2025-01-21 14:59:59 +0300

    fixing the error

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -2,12 +2,14 @@ import { Suspense } from "react";
 
 import { Info } from "../_components/Info";
 import { ActivityList } from "./_components/ActivityList"
+import { checkSubscription } from "@/lib/subscription";
 
-const ActivityPage = () => {
+const ActivityPage = async () => {
+  const isPro = await checkSubscription();
 
   return (
     <div className="w-full">
-      <Info />
+      <Info isPro={isPro}  />
       <Suspense fallback={<ActivityList.Skeleton />}>
       <ActivityList />
       </Suspense>
